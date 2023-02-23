5b9b73403a42a70fe24a4e1cb4fa3d163ca43b48
GrigoriiMartinkian <towaspas1@gmail.com>
2023-02-23 20:02:31 +0100

    finish-commit

--- src/SolutionTwo.java
+++ src/SolutionTwo.java
@@ -1,6 +1,20 @@
 public class SolutionTwo {
     public boolean isAnagram(String s, String t) {
+        
+        int k=0;
+     for(int i=0; i<s.length(); i++){
+       for(int j=0; j<t.length(); j++) {
 
+           if (s.charAt(i) == t.charAt(j)){
+               k++;
+               break;
+           }
+       }
+     }
+     if(k==s.length()){
+         return true;
+     }
+     return false;
     }
 
 }
