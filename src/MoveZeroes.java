08de27028ffe7aba5f747b2e006f6d4f8322d870
GrigoriiMartinkian <towaspas1@gmail.com>
2023-03-15 15:28:38 +0100

    finish-commit

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class MoveZeroes {
     public void moveZeroes(int[] nums) {
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=0; j<nums.length-1; j++) {
+                if (nums[j] == 0) {
+                    int result = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = result;
+                }
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i]+" ");
+        }
 
     }
 }
