2f044dad604fbe3086b937e96415132823e1540c
GrigoriiMartinkian <towaspas1@gmail.com>
2023-03-16 22:02:15 +0100

    finish-commit

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -13,9 +13,7 @@ public class MissingNumber {
         }
 
         for (int i=0;i < nums.length; i++) {  //[0,1,2,3,4,5,6,7,9]
-            if(nums[0]!=0){
-                return 0;
-            }
+
             if (i != nums[i]) {
                 return i;
             }
@@ -26,7 +24,7 @@ public class MissingNumber {
         }
         // for (int i = 0; i < nums.length; i++) {  //[0,1,2,3,4,5,6,7,9]
         //     int test=0;
-        //            if (i != nums[i]) {
+        //            if (test == nums[i]) {
         //                test++;
         //            }
         //            else{
